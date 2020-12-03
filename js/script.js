34eff351f9ba1c61d5d4ddf98d0afb0cb007e6e4
Simone Romiti <sr1992ei@gmail.com>
2020-12-03 15:25:46 +0100

    Mileston 1 completa

--- js/script.js
+++ js/script.js
@@ -5,3 +5,133 @@
 // Utilizzando la funzione forEach e il template
 // literal, visualizzare in pagina tutte le icone con il
 // proprio nome
+
+$(document).ready(
+  function(){
+
+
+    const icons = [
+      {
+        name: "dog",
+        prefix: "fa-",
+        type: "animal",
+        family: "fas"
+      },
+      {
+        name: "fish",
+        prefix: "fa-",
+        type: "animal",
+        family: "fas"
+      },
+      {
+        name: "tractor",
+        prefix: "fa-",
+        type: "vehicle",
+        family: "fas"
+      },
+      {
+        name: "hippo",
+        prefix: "fa-",
+        type: "animal",
+        family: "fas"
+      },
+      {
+        name: "spider",
+        prefix: "fa-",
+        type: "animal",
+        family: "fas"
+      },
+      {
+        name: "apple-alt",
+        prefix: "fa-",
+        type: "vegetable",
+        family: "fas"
+      },
+      {
+        name: "carrot",
+        prefix: "fa-",
+        type: "vegetable",
+        family: "fas"
+      },
+      {
+        name: "lemon",
+        prefix: "fa-",
+        type: "vegetable",
+        family: "fas"
+      },
+      {
+        name: "dragon",
+        prefix: "fa-",
+        type: "animal",
+        family: "fas"
+      },
+      {
+        name: "pepper-hot",
+        prefix: "fa-",
+        type: "vegetable",
+        family: "fas"
+      },
+      {
+        name: "space-shuttle",
+        prefix: "fa-",
+        type: "vehicle",
+        family: "fas"
+      },
+      {
+        name: "truck-monster",
+        prefix: "fa-",
+        type: "vehicle",
+        family: "fas"
+      },
+      {
+        name: "wheelchair",
+        prefix: "fa-",
+        type: "vehicle",
+        family: "fas"
+      },
+      {
+        name: "tram",
+        prefix: "fa-",
+        type: "vehicle",
+        family: "fas"
+      },
+      {
+        name: "fighter-jet",
+        prefix: "fa-",
+        type: "vehicle",
+        family: "fas"
+      },
+      {
+        name: "helicopter",
+        prefix: "fa-",
+        type: "vehicle",
+        family: "fas"
+      },
+    ];
+
+    const iconsContainer = $(".icons-container");
+    console.log(iconsContainer);
+    print(icons, iconsContainer);
+
+
+  }
+);
+
+
+//------------------------------------------------------//
+// FUNZIONI
+
+function print(array, container){
+  array.forEach(
+    (element, index) => {
+      $(container).append(`
+        <div class="icon">
+          <i class="${element.family} ${element.prefix}${element.name}"></i>
+          <div class="name">
+            ${element.name}
+          </div>
+        </div>
+      `)
+    }
+  );
+}
