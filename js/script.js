63340daa4bdf87886c2daa69af1c502bc93bba65
Simone Romiti <sr1992ei@gmail.com>
2020-12-03 15:04:55 +0100

    Struttura HTML CSS

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// milestone 1:
+// definire un array di oggetti; ogni oggetto
+// rappresenta un'icona, che è caratterizzata da:
+// nome, prefisso, tipo e famiglia.
+// Utilizzando la funzione forEach e il template
+// literal, visualizzare in pagina tutte le icone con il
+// proprio nome
